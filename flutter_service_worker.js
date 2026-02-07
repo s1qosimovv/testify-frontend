d4806e7ae2f4570bbae526c0ef09f1c896cbddea
s1qosimovv <s1.qosimovv@gmail.com>
2026-02-07 14:00:45 +0000

    Deploying to gh-pages from @ s1qosimovv/testify-frontend@ef2c75f43f1a428b33118ba1062ff9ca93e4e883 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -34,9 +34,9 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "canvaskit/canvaskit.js.symbols": "a3c9f77715b642d0437d9c275caba91e",
 "canvaskit/skwasm.js.symbols": "3a4aadf4e8141f284bd524976b1d6bdc",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "2c4d4410a4994aebea6b471d8e6ba18d",
+"flutter_bootstrap.js": "c2bee698964c73dacf382c84e8cb46d0",
 "version.json": "46969123a7f3583948d799f862a8f5b7",
-"main.dart.js": "00c0247cc0fbeec64a9e0657d2ff3a84"};
+"main.dart.js": "7613d1e33b9ff54cbf362151bae352f8"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
