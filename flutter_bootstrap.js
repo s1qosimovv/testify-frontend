893ff45533545c514855507c4c357c8f11ddc27a
s1qosimovv <s1.qosimovv@gmail.com>
2026-02-07 18:11:16 +0000

    Deploying to gh-pages from @ s1qosimovv/testify-frontend@8514eb72fe9a3478820c6892be3a38bc461079fc 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2877422442"
+    serviceWorkerVersion: "1983953081"
   }
 });
