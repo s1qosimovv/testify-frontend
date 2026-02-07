803bc64366d9294f161469b15c01468cf7c52d53
Sardorbek Qosimov <s1qosimovv@gmail.com>
2026-02-07 17:39:48 +0500

    Update API base URL to live Render server

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -2,11 +2,7 @@ import 'package:flutter/foundation.dart';
 
 class ApiConstants {
   // 10.0.2.2 for Android Emulator, localhost for Web/iOS
-  static String get baseUrl {
-    if (kIsWeb) return 'http://127.0.0.1:8000/api';
-    if (defaultTargetPlatform == TargetPlatform.android) return 'http://10.0.2.2:8000/api';
-    return 'http://localhost:8000/api';
-  }
+  static String get baseUrl => 'https://testify-backend-sbb4.onrender.com/api';
   
   static String get uploadFile => '$baseUrl/upload-file';
   static String get generateQuiz => '$baseUrl/generate-quiz';
